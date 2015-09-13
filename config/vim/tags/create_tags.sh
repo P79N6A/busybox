55bba50db0ad13a07ee5491065826055469ead7f
Elwin.Gao <Elwin.Gao4444@gmail.com>
2015-09-13 10:45:09 +0800

    update create tags for vim

--- config/vim/tags/create_tags.sh
+++ config/vim/tags/create_tags.sh
@@ -3,12 +3,13 @@
 rm -f *tags
 
 CTAGS='ctags'
-${CTAGS} -f phptags ./php_funclist
+# ${CTAGS} -f phptags ./php_funclist
 
-CTAGS=${CTAGS}' --c++-kinds=+p --fields=+iaS --extra=+q+f --tag-relative=yes --language-force=C++'
-${CTAGS} -f cpptags -R /usr/include/c++ -R ~/.vim/cpp_src
+CTAGS_CPP=${CTAGS}' --c++-kinds=+p --fields=+iaS --extra=+q+f --tag-relative=yes --language-force=C++'
+${CTAGS_CPP} -f cpptags -R /usr/include/c++
+${CTAGS_CPP} -f cppexttags -R ~/.vim/cpp_src
 
-CTAGS2=${CTAGS}' -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ -I __restrict=restrict -I  __const=const -I ((1, -I 3))'
-${CTAGS2} -f ctags /usr/include/*.h
-${CTAGS2} -f gnutags -R /usr/include/x86_64-linux-gnu
-${CTAGS2} -f kerneltags -R /usr/include/linux
+CTAGS_C=${CTAGS}' -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ -I __restrict=restrict -I  __const=const -I ((1, -I 3))'
+${CTAGS_C} -f ctags /usr/include/*.h
+${CTAGS_C} -f gnutags -R /usr/include/x86_64-linux-gnu
+${CTAGS_C} -f kerneltags -R /usr/include/linux
