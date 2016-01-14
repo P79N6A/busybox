9b2f1d44ff8adccdffbd01b60124568b665330fc
dokey4444 <elwin.gao4444@gmail.com>
2016-01-14 11:43:06 +0800

    update deom: pthread_key

--- library-demo/pthread/pthread_key.c
+++ library-demo/pthread/pthread_key.c
@@ -32,9 +32,18 @@ void* pthread_fun(void *arg)
 	int no = (int)(long)arg;
 	int key = no + 100;
 	int *p = 0;
-	pthread_setspecific(pgkey, &key);
+	if (no != 4) {
+		if (pthread_setspecific(pgkey, &key) != 0) {
+			printf("no: %d, key set error.\n", no);
+			return NULL;
+		}
+	}
 	printf("pthread_fun: %d\n", no);
 	p = pthread_getspecific(pgkey);
+	if (p == NULL) {
+		printf("no: %d, key not found.\n", no);
+		return NULL;
+	}
 	printf("pthread_key: %d\n", *p);
 	return NULL;
 }
@@ -47,14 +56,16 @@ void* pthread_fun(void *arg)
  */
 int main(int argc, char *argv[])
 {
-	pthread_t tid1, tid2, tid3;
+	pthread_t tid1, tid2, tid3, tid4;
 	pthread_key_create(&pgkey, key_create);
 	pthread_create(&tid1, NULL, pthread_fun, (void*)(long)1);
 	pthread_create(&tid2, NULL, pthread_fun, (void*)(long)2);
 	pthread_create(&tid3, NULL, pthread_fun, (void*)(long)3);
+	pthread_create(&tid4, NULL, pthread_fun, (void*)(long)4);
 	pthread_join(tid1, NULL);
 	pthread_join(tid2, NULL);
 	pthread_join(tid3, NULL);
+	pthread_join(tid4, NULL);
 	pthread_key_delete(pgkey);
 
 	return EXIT_SUCCESS;
