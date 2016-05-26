8c77bbd2f7d1ca8f7bd35919e94016656f19c44b
dokey4444 <elwin.gao4444@gmail.com>
2016-05-26 16:58:20 +0800

    update sqlite

---
+++ DataBase/SQLite/main.cpp
@@ -0,0 +1,32 @@
+/*
+// =====================================================================================
+// 
+//       Filename:  main.cpp
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  05/26/2016 04:55:28 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Elwin.Gao (elwin), elwin.gao4444@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+/* 
+// ===  FUNCTION  ======================================================================
+//         Name:  main
+//  Description:  
+// =====================================================================================
+*/
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello World" << std::endl;
+	return EXIT_SUCCESS;
+}				// ----------  end of function main  ----------
