20d8b6931021eda123eb2447491a0ace9ba5cc3d
dokey4444 <elwin.gao4444@gmail.com>
2016-06-06 20:48:59 +0800

    update

--- demo/hadoop/ZooKeeper/java_demo/main.java
+++ demo/hadoop/ZooKeeper/java_demo/main.java
@@ -25,29 +25,50 @@ public class main {
 						System.out.println("已经触发了" + event.getType() + "事件！");
 					}
 				});
-		// 创建一个目录节点
-		zk.create("/testRootPath", "testRootData".getBytes(), Ids.OPEN_ACL_UNSAFE,
-				CreateMode.PERSISTENT);
-		// 创建一个子目录节点
-		zk.create("/testRootPath/testChildPathOne", "testChildDataOne".getBytes(),
-				Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
-		System.out.println(new String(zk.getData("/testRootPath",false,null)));
-		// 取出子目录节点列表
-		System.out.println(zk.getChildren("/testRootPath",true));
-		// 修改子目录节点数据
-		zk.setData("/testRootPath/testChildPathOne","modifyChildDataOne".getBytes(),-1);
-		System.out.println("目录节点状态：["+zk.exists("/testRootPath",true)+"]");
-		// 创建另外一个子目录节点
-		zk.create("/testRootPath/testChildPathTwo", "testChildDataTwo".getBytes(),
-				Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
-		System.out.println(new String(zk.getData("/testRootPath/testChildPathTwo",true,null)));
-		// 删除子目录节点
-		zk.delete("/testRootPath/testChildPathTwo",-1);
-		zk.delete("/testRootPath/testChildPathOne",-1);
-		// 删除父目录节点
-		zk.delete("/testRootPath",-1);
+		System.out.println("new zookeeper");
+
+//		// 创建一个目录节点
+//		zk.create("/testRootPath", "testRootData".getBytes(), Ids.OPEN_ACL_UNSAFE,
+//				CreateMode.PERSISTENT);
+//		System.out.println("create: /testRootPath");
+//
+//		// 创建一个子目录节点
+//		zk.create("/testRootPath/testChildPathOne", "testChildDataOne".getBytes(),
+//				Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+//		System.out.println("create: /testRootPath/testChildPathOne");
+//		System.out.println(new String(zk.getData("/testRootPath",false,null)));
+//		System.out.println("get: /testRootPath");
+//
+//		// 取出子目录节点列表
+//		System.out.println(zk.getChildren("/testRootPath",true));
+//		System.out.println("get children: /testRootPath");
+//
+//		// 修改子目录节点数据
+//		zk.setData("/testRootPath/testChildPathOne","modifyChildDataOne".getBytes(),-1);
+//		System.out.println("目录节点状态：["+zk.exists("/testRootPath",true)+"]");
+//		System.out.println("set: /testRootPath/testChildPathOne");
+//
+//		// 创建另外一个子目录节点
+//		zk.create("/testRootPath/testChildPathTwo", "testChildDataTwo".getBytes(),
+//				Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+//		System.out.println("create: /testRootPath/testChildPathTwo");
+//		System.out.println(new String(zk.getData("/testRootPath/testChildPathTwo",true,null)));
+//		System.out.println("get: /testRootPath/testChildPathTwo");
+//
+//		// 删除子目录节点
+//		zk.delete("/testRootPath/testChildPathTwo",-1);
+//		System.out.println("delete: /testRootPath/testChildPathTwo");
+//		zk.delete("/testRootPath/testChildPathOne",-1);
+//		System.out.println("delete: /testRootPath/testChildPathOne");
+//
+//		// 删除父目录节点
+//		zk.delete("/testRootPath",-1);
+//		System.out.println("delete: /testRootPath");
+
 		// 关闭连接
 		zk.close();
+		System.out.println("close zookeeper");
+
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
