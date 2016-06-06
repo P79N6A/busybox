7f4112619db633c103f8d3cecbc2b47d53a6ea49
dokey4444 <elwin.gao4444@gmail.com>
2016-06-06 14:38:17 +0800

    update

--- demo/hadoop/ZooKeeper/c++_lib/zookeeper_class.hpp
+++ demo/hadoop/ZooKeeper/c++_lib/zookeeper_class.hpp
@@ -41,6 +41,10 @@ namespace zkclass
 	{
 	public:
 		using byte = unsigned char;
+
+		enum State
+		{
+		};
 		// ====================  LIFECYCLE     =======================================
 
 		/* 
@@ -142,6 +146,314 @@ namespace zkclass
 		// =====================================================================================
 		*/
 		void create(string path, byte data[], vector<ACL> acl, CreateMode createMode, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  remove
+		//  Description:  Remove the node with the given path.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void remove(string path, int version);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  remove
+		//  Description:  The asynchronous version of remove.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void remove(string path, int version, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  exists
+		//  Description:  Return the stat of the node of the given path.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		Stat exists(string path, bool watch);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  exists
+		//  Description:  The asynchronous version of exists.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void exists(string path, bool watch, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  exists
+		//  Description:  Return the stat of the node of the given path.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		Stat exists(string path, Watcher watcher);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  exists
+		//  Description:  The asynchronous version of exists.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void exists(string path, Watcher watcher, AsyncCallback cb, void *ctx);
+		
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_acl
+		//  Description:  Return the ACL and stat of the node of the given path.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		vector<ACL> get_acl(string path, Stat stat);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_acl
+		//  Description:  The asynchronous version of get_acl.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void get_acl(string path, Stat stat, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_children
+		//  Description:  Return the list of the children of the node of the given path.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		vector<string> get_children(string path, bool watch);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_children
+		//  Description:  The asynchronous version of getChildren.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void get_children(string path, bool watch, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_children
+		//  Description:  For the given znode path return the stat and children list.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		vector<string> get_children(string path, bool watch, Stat stat);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_children
+		//  Description:  Return the list of the children of the node of the given path.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		vector<string> get_children(string path, Watcher watcher);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_children
+		//  Description:  The asynchronous version of getChildren.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void get_children(string path, Watcher watcher, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_children
+		//  Description:  For the given znode path return the stat and children list.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void get_children(string path, Watcher watcher, Stat stat);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_data
+		//  Description:  The asynchronous version of getData.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void get_data(string path, bool watch, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_data
+		//  Description:  Return the data and the stat of the node of the given path.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		byte* get_data(string path, bool watch, Stat stat);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_data
+		//  Description:  The asynchronous version of getData.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void get_data(string path, Watcher watcher, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_data
+		//  Description:  Return the data and the stat of the node of the given path.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		byte* get_data(string path, Watcher watcher, Stat stat);
+		
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_session_id
+		//  Description:  The session id for this ZooKeeper client instance.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		long get_session_id();
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_session_passwd
+		//  Description:  The session password for this ZooKeeper client instance.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		byte* get_session_passwd();
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_session_timeout
+		//  Description:  The negotiated session timeout for this ZooKeeper client instance.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		int get_session_timeout();
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_state
+		//  Description:   
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		ZooKeeper::State get_state();
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  register
+		//  Description:  Specify the default watcher for the connection
+		//                (overrides the one specified during construction).
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void register_watcher(Watcher watcher);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  set_acl
+		//  Description:  Set the ACL for the node of the given path if such a node exists
+		//                and the given version matches the version of the node.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		Stat set_acl(string path, vector<ACL> acl, int version);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  set_acl
+		//  Description:  The asynchronous version of setACL.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void set_acl(string path, vector<ACL> acl, int version, AsyncCallback cb, void *ctx);
+		
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  set_data
+		//  Description:  Set the data for the node of the given path if such a node exists
+		//                and the given version matches the version of the node
+		//                (if the given version is -1, it matches any node's versions).
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		Stat set_data(string path, byte data[], int version);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  set_data
+		//  Description:  The asynchronous version of setData.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void set_data(string path, byte data[], int version, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  sync
+		//  Description:  Asynchronous sync.
+		//   Parameters:  
+		//  ReturnValue:  
+		// =====================================================================================
+		*/
+		void sync(string path, AsyncCallback cb, void *ctx);
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  get_sasl_client
+		//  Description:   
+		// =====================================================================================
+		*/
+		void get_sasl_client();
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  multi
+		//  Description:   
+		// =====================================================================================
+		*/
+		void multi();
+
+		/* 
+		// ===  FUNCTION  ======================================================================
+		//         Name:  transaction
+		//  Description:   
+		// =====================================================================================
+		*/
+		void transaction();
 
 		// ====================  ACCESSORS     =======================================
 
