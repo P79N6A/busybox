e401760307e897f76052e02ec7f0d4cbf129a95b
dokey4444 <elwin.gao4444@gmail.com>
2016-06-23 00:11:21 +0800

    update

--- demo/c++/c++11/thread.cpp
+++ demo/c++/c++11/thread.cpp
@@ -17,22 +17,32 @@
 */
 
 #include <cstdlib>
+#include <cassert>
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include <chrono>
+
+std::once_flag flag;
 
 void fun0()
 {
+	std::call_once(flag, [](){ std::cout << "Simple example: called once\n"; });	// 共用一个flag的call_once函数全程只会调用一次
+
 	std::cout << "fun() in new thread" << std::endl;
 }
 
 void fun1(int i)
 {
+	std::call_once(flag, [](){ std::cout << "Simple example: called once\n"; });	// 共用一个flag的call_once函数全程只会调用一次
+
 	std::cout << "fun(int i) in new thread, i = " << i << std::endl;
 }
 
 void fun()
 {
+	std::call_once(flag, [](){ std::cout << "Simple example: called once\n"; });	// 共用一个flag的call_once函数全程只会调用一次
+
 	// 这里的std::this_thread是一个namespace而不是一个对象，感觉怪怪的
 	std::cout << "thread id: " << std::this_thread::get_id() << std::endl;
 	std::cout << "core num: " << std::thread::hardware_concurrency() << std::endl;
@@ -61,6 +71,22 @@ int main(int argc, char *argv[])
 	// 获取thread相关信息
 	std::thread t(fun);
 	t.join();
+
+	// mutex用法
+	// 另外还有metex的可递归版本： recursive_mutex
+	std::mutex mtx;
+	mtx.lock();
+	assert(mtx.try_lock() == false);
+	mtx.unlock();
+	std::lock_guard<std::mutex> guard_mtx(mtx);	// 常见的用法是配合lock_guard一起使用
+
+	std::timed_mutex tmtx;
+	tmtx.lock();
+	assert(tmtx.try_lock() == false);
+	assert(tmtx.try_lock_for(std::chrono::milliseconds(10)) == false);
+	assert(tmtx.try_lock_until(std::chrono::steady_clock::now() + std::chrono::milliseconds(10)) == false);
+	tmtx.unlock();
+	std::lock_guard<std::timed_mutex> guard_tmtx(tmtx);	// 常见的用法是配合lock_guard一起使用
 
 	std::cout << "Hello World" << std::endl;
 	return EXIT_SUCCESS;
