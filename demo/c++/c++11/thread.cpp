0007364e9862ff5bb00c0962c1b2944eb40cc4bd
dokey4444 <elwin.gao4444@gmail.com>
2016-06-23 14:16:03 +0800

    update

--- demo/c++/c++11/thread.cpp
+++ demo/c++/c++11/thread.cpp
@@ -21,6 +21,7 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <atomic>
 #include <chrono>
 
 std::once_flag flag;
@@ -79,7 +80,13 @@ int main(int argc, char *argv[])
 	assert(mtx.try_lock() == false);
 	mtx.unlock();
 	std::lock_guard<std::mutex> guard_mtx(mtx);	// 常见的用法是配合lock_guard一起使用
+	// lock_guard还可以配合std::adopt_lock参数使用，用于承接已经lock的mutex
+	// 这里的用法是有问题的mtx由于被两个guard_mtx所管理，虽然std::adopt_lock参数不会再次lock
+	// 但是，会出现两次unlock的情况，官方文档中对于unlock两次的行为是未定义的
+//	std::lock_guard<std::mutex> guard_mtx2(mtx, std::adopt_lock);
 
+	// timed_mutex用法
+	// 另外还有metex的可递归版本： recursive_timed_mutex
 	std::timed_mutex tmtx;
 	tmtx.lock();
 	assert(tmtx.try_lock() == false);
@@ -87,6 +94,40 @@ int main(int argc, char *argv[])
 	assert(tmtx.try_lock_until(std::chrono::steady_clock::now() + std::chrono::milliseconds(10)) == false);
 	tmtx.unlock();
 	std::lock_guard<std::timed_mutex> guard_tmtx(tmtx);	// 常见的用法是配合lock_guard一起使用
+
+	// lock_guard用法简单，性能良好，但有时会显得功能不足，此时std::unique_lock可以提供更多的功能
+	// unique_lock可以使用4种不同的参数形式（lock_guard只有两种：默认和std::adopt_lock）：
+	// 策略 		tag type 		描述
+	// (默认) 		无 			请求锁，阻塞当前线程直到成功获得锁。
+	// std::defer_lock 	std::defer_lock_t 	不请求锁。
+	// std::try_to_lock 	std::try_to_lock_t 	尝试请求锁，但不阻塞线程，锁不可用时也会立即返回。
+	// std::adopt_lock 	std::adopt_lock_t 	假定当前线程已经获得互斥对象的所有权，所以不再请求锁。
+	// 
+	// 另外，unique_lock的另一种非常重要的用法是作为返回值将锁传递出去，这一点是lock_guard做不到的
+	std::mutex mtx_uniq;
+	std::unique_lock<std::mutex> uniq_lock(mtx_uniq, std::defer_lock);	//这是一种只创建，不加锁的初始化方式
+
+	// 当需要同时对多个mutex进行加锁的时候，传统的方法是将加锁顺序固定，以避免死锁，
+	// 新的方式是使用std::lock(std::try_lock)进行一次事物性的连续加锁
+	// 由于并没有std::unlock方法，所以解锁过程需要手动一个个的unlock，一般使用lock_guard和unique_lock来帮助进行自动解锁
+	std::mutex mtx1, mtx2, mtx3;
+	std::lock(mtx1, mtx2, mtx3);	// 先锁后guard的方式
+	std::lock_guard<std::mutex> lock1(mtx1, std::adopt_lock);
+	std::lock_guard<std::mutex> lock2(mtx2, std::adopt_lock);
+	std::lock_guard<std::mutex> lock3(mtx3, std::adopt_lock);
+
+	std::mutex mtx4, mtx5, mtx6;
+	std::unique_lock<std::mutex> lock4(mtx4, std::defer_lock);
+	std::unique_lock<std::mutex> lock5(mtx5, std::defer_lock);
+	std::unique_lock<std::mutex> lock6(mtx6, std::defer_lock);
+	std::lock(mtx4, mtx5, mtx6);	// 先guard后锁的方式
+
+	// 原子变量
+	// 原子变量的用法十分简单，但是接口众多，详细内容参考：
+	// http://en.cppreference.com/w/cpp/atomic/atomic
+	std::atomic<int> i(0);	// 原子变量的使用和普通int类型一样，基本没有差别
+	std::atomic<int> j(1);
+	i=1; ++i; --i; i+=2; i+=j;
 
 	std::cout << "Hello World" << std::endl;
 	return EXIT_SUCCESS;
